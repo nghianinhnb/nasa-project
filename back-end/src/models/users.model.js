71dbdfea8ea9ff521d32af681626a5b7263baa26
NGHIA.NV190060@sis.hust.edu.vn <NGHIA.NV190060@sis.hust.edu.vn>
2022-08-03 08:04:25 +0700

    be-upd: add name field for user model

--- back-end/src/models/users.model.js
+++ back-end/src/models/users.model.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
     email: {
         type: String,
         required: true,
     },
     passwordHash: {
         type: String,
-        required: true,
     },
     salt: {
         type: String,
-        required: true,
     }
 })
 
