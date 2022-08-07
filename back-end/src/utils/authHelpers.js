0679da050bb286363d9599b0c663817cb954738c
NGHIA.NV190060@sis.hust.edu.vn <NGHIA.NV190060@sis.hust.edu.vn>
2022-08-07 15:20:49 +0700

    be-feat: auth helper hold auth crypto function

---
+++ back-end/src/utils/authHelpers.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+
+exports.generateAccessToken = (userId) => {
+    return jwt.sign({ id: userId, exp: Math.floor(Date.now() * 0.001) + 604800 }, process.env.ACCESS_TOKEN_KEY);
+}
+
+
+exports.generateRefreshToken = (exp=null) => {
+    if (exp) return `${exp} ${crypto.randomBytes(128).toString('base64')}`;
+    return `${Math.floor(Date.now() * 0.001) + 31536000} ${crypto.randomBytes(128).toString('base64')}`;
+}
+
+
+exports.verifyRefreshToken = (refreshToken) => {
+    const [exp, token] = refreshToken.split(' ', 2);
+    if (!token || exp < Math.floor(Date.now() * 0.001)) return;
+    return {exp, token}
+}
+
+
+exports.storingTokenScript = (accessToken, refreshToken) => {
+    return `<script>localStorage.setItem('accessToken','${accessToken}');localStorage.setItem('refreshToken','${refreshToken}');location.href='/'</script>`;
+}
+
+
+exports.generateSalt = (length) => {
+    return crypto.randomBytes(length).toString('hex');
+}
+
+
+exports.hashPassword = (password, salt='') => {
+    return crypto.createHash('sha256').update(password + salt).digest('base64')
+}
