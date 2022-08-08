900130d1b38ec09345bf7bfba4dcadeeb16d4b68
NGHIA.NV190060@sis.hust.edu.vn <NGHIA.NV190060@sis.hust.edu.vn>
2022-08-08 22:44:26 +0700

    be-chore: connect to test db when test

--- back-end/__test__/launches.test.js
+++ back-end/__test__/launches.test.js
@@ -1,12 +1,21 @@
+const mongoose = require('mongoose');
 const supertest = require('supertest');
 const expressApp = require('../src/app');
-const constants = require('../src/config/constants')
+const constants = require('../src/config/constants');
 
 
 const app = supertest(expressApp);
 
 
 describe(`Test GET ${constants.PATH_LAUNCH}`, () => {
+    beforeAll(async () => {
+        await mongoose.connect(process.env.MONGO_URL);
+    });
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+    });
+
     test('It should respond with 200', async () => {
         const res = await app.get(constants.PATH_LAUNCH)
                              .expect(401);
