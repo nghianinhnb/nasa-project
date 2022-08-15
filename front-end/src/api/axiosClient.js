f7ffaf63897e6d61d399bc1bf588999522c13994
NGHIA.NV190060@sis.hust.edu.vn <NGHIA.NV190060@sis.hust.edu.vn>
2022-08-15 18:20:36 +0700

    fe-fix: add prefix for refresh token url

--- front-end/src/api/axiosClient.js
+++ front-end/src/api/axiosClient.js
@@ -49,7 +49,7 @@ axiosClient.interceptors.response.use(
         config.url !== "/sign-in" && config.url !== "/refresh-token" &&
         error.response && error.response.status === 401
       ) {
-        let res = await axios.post((process.env.REACT_APP_API_URL || '') + '/refresh-token', {refreshToken: Global.refreshToken});
+        let res = await axios.post((process.env.REACT_APP_API_URL || 'api/v1') + '/refresh-token', {refreshToken: Global.refreshToken});
 
         const {result, accessToken, refreshToken} = res.data;
         if (result==='success') {
